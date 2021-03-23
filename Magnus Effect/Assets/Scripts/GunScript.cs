49fa6019f2f54daf730ff9600fefe297aeedcdf5
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-03-23 17:35:30 +0200

    update

--- Magnus Effect/Assets/Scripts/GunScript.cs
+++ Magnus Effect/Assets/Scripts/GunScript.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunScript : MonoBehaviour {
+public class GunScript : MonoBehaviour
+{
 
 	[SerializeField]
 	private GameObject bullet;
@@ -12,23 +13,25 @@ public class GunScript : MonoBehaviour {
 
 	[SerializeField]
 	private float force;
+	[SerializeField]
+	private float deltaTime = 1f;
 
 	private float _time;
 
-    private void Awake()
-    {
+	private void Awake()
+	{
 		_time = Time.time;
 	}
 
-    void Update()
+	void Update()
 	{
-        if (Time.time - _time > 1f)
+		if (Time.time - _time > deltaTime)
 		{
 			GameObject go = (GameObject)Instantiate(bullet, bulletStartPos.position, Quaternion.identity);
 			go.GetComponent<Rigidbody>().AddForce(Vector3.right * force, ForceMode.Impulse);
 			_time = Time.time;
-        }
-		if(Input.GetKeyDown(KeyCode.Z))
+		}
+		if (Input.GetKeyDown(KeyCode.Z))
 		{
 			GameObject go = (GameObject)Instantiate(bullet, bulletStartPos.position, Quaternion.identity);
 			go.GetComponent<Rigidbody>().AddForce(Vector3.right * force, ForceMode.Impulse);
